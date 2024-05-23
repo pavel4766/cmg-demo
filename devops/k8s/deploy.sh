457611f589fe9b5feaeb3604170655fe185ec765
Pavel Marchenko <pavel.marchenko@unity3d.com>
2024-05-22 20:34:26 -0700

    deployment changes

--- devops/k8s/deploy.sh
+++ devops/k8s/deploy.sh
@@ -5,6 +5,7 @@ export $(sops -d .env | xargs)
 
 envsubst < ./backend-secrets.yaml.tpl > ./backend-secrets.yaml
 
+kubectl delete deployment cmg-backend-deployment 
 kubectl apply -f ./backend-secrets.yaml
 kubectl apply -f ./cmg-backend-deployment.yaml
 kubectl apply -f ./cmg-backend-lb.yaml
