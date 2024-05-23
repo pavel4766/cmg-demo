d6be23950b9b24f54caab3d101794fe59d295b84
Pavel Marchenko <pavel.y.marchenko@gmail.com>
2024-05-23 05:13:48 -0700

    node groups back to t3 small

--- devops/terraform/main.tf
+++ devops/terraform/main.tf
@@ -74,7 +74,7 @@ module "eks" {
     one = {
       name = "node-group-1"
 
-      instance_types = ["t3.large"]
+      instance_types = ["t3.small"]
 
       min_size     = 1
       max_size     = 3
@@ -84,7 +84,7 @@ module "eks" {
     two = {
       name = "node-group-2"
 
-      instance_types = ["t3.large"]
+      instance_types = ["t3.small"]
 
       min_size     = 1
       max_size     = 2
